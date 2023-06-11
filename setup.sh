51d4ba6ce2010b1fae75d5256d2658274e6fdc04
sinkthemall <tphat9a2@gmail.com>
2023-06-11 21:42:19 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,32 @@
+sudo apt install python3
+# install python3
+
+sudo apt install sagemath
+# install sagemath, this could take really long as sagemath about 3~4gb
+
+sudo apt install ncat
+# install ncat for some oracle challenge
+
+# install some neccessary modules for python
+pip3 install pwntools
+# install pwntools
+pip3 install primefac
+# install primefac
+pip3 install pycryptodome
+# install pycryptodome
+pip3 install sympy
+# install sympy
+pip3 install gmpy2
+# install gmpy2
+pip3 install numpy
+# install numpy
+pip3 install numba
+# install numba, a superstrong precompile for python, optimize the speed as same as C/C++
+sage --pip install pycryptodome
+# install pycryptodome for sagemath
+sage --pip install pwntools
+# install pwntools for sagemath
+
+
+
+echo "install completed!!!"
